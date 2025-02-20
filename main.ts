1c7f984acfad6be95ba2e802f5e556f6f23759fd
minecraftMattis <mattis.lueger@icloud.com>
2025-02-20 17:57:54 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(1)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(-1)
+})
+basic.forever(function () {
+	
+})
