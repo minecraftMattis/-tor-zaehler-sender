cb390285266d51d900d24c387618c0c05bd5b280
minecraftMattis <mattis.lueger@icloud.com>
2025-02-20 18:01:30 +0100

    funkgrupe hinzugefügt

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 input.onButtonPressed(Button.A, function () {
+    radio.setGroup(0)
     radio.sendNumber(1)
 })
 input.onButtonPressed(Button.B, function () {
+    radio.setGroup(1)
     radio.sendNumber(-1)
 })
 basic.forever(function () {
