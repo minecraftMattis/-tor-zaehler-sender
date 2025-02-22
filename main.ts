b44ad26404547695113822ea59579cc7f62872ef
minecraftMattis <mattis.lueger@icloud.com>
2025-02-22 10:52:26 +0100

    + und - eingefuegt

--- main.ts
+++ main.ts
@@ -1,11 +1,27 @@
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    if (zeichen == "+") {
+        zeichen = "-"
+    } else if (zeichen == "-") {
+        zeichen = "+"
+    }
+})
 input.onButtonPressed(Button.A, function () {
     radio.setGroup(0)
-    radio.sendNumber(1)
+    sendeZahl()
 })
 input.onButtonPressed(Button.B, function () {
     radio.setGroup(1)
-    radio.sendNumber(-1)
+    sendeZahl()
 })
+function sendeZahl () {
+    if (zeichen == "+") {
+        radio.sendNumber(1)
+    } else if (zeichen == "-") {
+        radio.sendNumber(-1)
+    }
+}
+let zeichen = ""
+zeichen = "+"
 basic.forever(function () {
-	
+    basic.showString(zeichen)
 })
